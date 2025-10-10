4edbeaf4d738c261935319bf7f6b990a527ed319
serdarch1k <sardormadgoziyev@gmail.com>
2025-10-10 11:22:21 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http"); // core module
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -15,8 +26,17 @@ app.set("view engine", "ejs");
 // ejs = backend da view (html frontend) ni yasash uchun
 
 // 4: Routing code
-app.get("/hello", function (req, res) {
-    res.end("<h1>Hello World!</h1>");
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+    res.render("harid");
 });
 
 const server = http.createServer(app);
