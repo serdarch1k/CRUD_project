8d1f3bce131d59b983a705562fdc97f2409e4ea3
serdarch1k <sardormadgoziyev@gmail.com>
2025-10-26 19:10:30 +0900

    feat: FrontEnd JavaScriptni qurish

--- app.js
+++ app.js
@@ -19,15 +19,9 @@ app.set("view engine", "ejs");
 // 4: Routing code (endpoints)
 app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
-    console.log(req.body);
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end("something went wrong");
-        } else {
-            res.end("successfully added");
-        }
+        res.json(data.ops[0]);
     });
 });
 
---
+++ public/browser.js
@@ -0,0 +1,35 @@
+console.log("FrontEnd JS ishga tushdi");
+
+function itemTemplate(item) {
+   return `<li 
+      class="list-group-item list-group-item-info d-flex align-items-center justify-content-between mb-2" style="border-radius: 8px;">
+      <span class="item-text">${item.reja}</span>
+      <div>
+         <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">
+               O'zgartirish
+         </button>
+         <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">
+               O'chirish
+         </button>
+      </div>
+   </li>`;
+}
+
+let createField = document.getElementById("create-field");
+
+document.getElementById("create-form").addEventListener("submit", function (e) {
+   e.preventDefault();  // boshqa page ga o'tib ketmasligi uchun
+
+   axios  // modern requiest
+      .post("/create-item", { reja: createField.value })
+      .then((response) => {
+         document
+            .getElementById("item-list")
+            .insertAdjacentHTML("beforeend", itemTemplate(response.data));
+         createField.value = "";
+         createField.focus();
+      })
+      .catch((err) => {
+         console.log("Iltimos qaytadan harakat qiling!");
+      });
+});
