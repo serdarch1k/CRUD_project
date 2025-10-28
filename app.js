29d6d5f59f44b50c28e75f14ff818307804b61b5
serdarch1k <sardormadgoziyev@gmail.com>
2025-10-28 12:20:12 +0900

    feat: Reja loyihamning yakuniy bosqich logic

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 // MongoDB chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 // 1: Kirish code
 app.use(express.static("public"));                                   // Middleware DP
@@ -23,6 +24,35 @@ app.post("/create-item", (req, res) => {
     db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
         res.json(data.ops[0]);
     });
+});
+
+app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans").deleteOne(
+        { _id: new mongodb.ObjectId(id) },
+        function (err, data) {
+            res.json({ state: "success" });
+        }
+    );
+});
+
+app.post("/edit-item", (req, res) => {
+    const data = req.body;
+    db.collection("plans").findOneAndUpdate(
+        { _id: new mongodb.ObjectId(data.id) },
+        { $set: { reja: data.new_input } },
+        function (err, data) {
+            res.json({ state: "success" });
+        }
+    );
+});
+
+app.post("/delete-all", (req, res) => {
+    if (req.body.delete_all) {
+        db.collection("plans").deleteMany(function () {
+            res.json({ state: "Deleted all plans" });
+        });
+    }
 });
 
 app.get("/", function (req, res) {
--- public/browser.js
+++ public/browser.js
@@ -18,11 +18,12 @@ function itemTemplate(item) {
 let createField = document.getElementById("create-field");
 
 document.getElementById("create-form").addEventListener("submit", function (e) {
-   e.preventDefault();  // boshqa page ga o'tib ketmasligi uchun
+   e.preventDefault();  // boshqa page ga o'tib ketmasligi uchun (Traditional API ni to'xtatadi)
 
-   axios  // modern requiest
+   axios  // modern requiest | Rest API
       .post("/create-item", { reja: createField.value })
       .then((response) => {
+         console.log("response:", response);
          document
             .getElementById("item-list")
             .insertAdjacentHTML("beforeend", itemTemplate(response.data));
@@ -32,4 +33,54 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
       .catch((err) => {
          console.log("Iltimos qaytadan harakat qiling!");
       });
+});
+
+document.addEventListener("click", function (e) {
+   // delete oper
+   console.log(e.target);
+   if (e.target.classList.contains("delete-me")) {
+      if (confirm("Aniq o'chirmoqchimisiz?")) {
+         axios
+            .post("/delete-item", { id: e.target.getAttribute("data-id") })
+            .then((response) => {
+               console.log(response.data);
+               e.target.parentElement.parentElement.remove();
+            })
+            .catch((err) => {
+               console.log("Iltimos qaytadan harakat qiling!");
+            });
+      }
+   }
+
+   // edit oper
+   if (e.target.classList.contains("edit-me")) {
+      let userInput = prompt(
+         "O'zgartirish kiriting",
+         e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+      );
+      if (userInput) {
+         axios
+            .post("/edit-item", {
+               id: e.target.getAttribute("data-id"),
+               new_input: userInput,
+            })
+            .then((response) => {
+               console.log(response.data);
+               e.target.parentElement.parentElement.querySelector(
+                  ".item-text"
+               ).innerHTML = userInput;
+            })
+            .catch((err) => {
+               console.log("Iltimos qaytadan harakat qiling!");
+            });
+      }
+   }
+});
+
+// delete all
+document.getElementById("clean-all").addEventListener("click", function () {
+   axios.post("/delete-all", { delete_all: true }).then((response) => {
+      alert(response.data.state);
+      document.location.reload();
+   });
 });
